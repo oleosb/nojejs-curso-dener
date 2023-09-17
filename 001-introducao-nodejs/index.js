0384ae24381f75241c817249a18dc6d3d3910860
oleosb <oleosbo@gmail.com>
2023-09-17 13:04:47 -0300

    Primeira Rota

--- 001-introducao-nodejs/index.js
+++ 001-introducao-nodejs/index.js
@@ -1,1 +1,16 @@
-console.log(4000)
+import http from "node:http";
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 404;
+
+  return res.end(
+    JSON.stringify({
+      status: 404,
+      body: "404 (Not Found)",
+    })
+  );
+});
+
+server.listen(3000, () => {
+  console.log("Deu bom");
+});
