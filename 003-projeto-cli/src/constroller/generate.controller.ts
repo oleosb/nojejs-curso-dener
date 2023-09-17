3e36703b729d56ad14970f4a549bd8f7faee7caf
oleosb <oleosbo@gmail.com>
2023-09-17 17:14:36 -0300

    Gerador de git clone

--- 003-projeto-cli/src/constroller/generate.controller.ts
+++ 003-projeto-cli/src/constroller/generate.controller.ts
@@ -1,6 +1,10 @@
+import shellJs from 'shelljs';
+
 import { EChoicesBoilerPlate } from 'enum/choices-boilerplate.enum';
 import { EGitName } from 'enum/git-name.enum';
 import { IAnswers } from 'interface/answers.interface';
+import path from 'node:path';
+import fs from 'node:fs';
 
 class GenerateController {
   public gen(answers: IAnswers) {
@@ -19,7 +23,19 @@ class GenerateController {
     }
   }
   private _execPath(gitName: string, folderName: string) {
-    console.log(gitName, folderName);
+    try {
+      shellJs.cd(path.resolve());
+      shellJs.exec(`git clone git@github.com:troquatte/${gitName}.git`);
+
+      fs.renameSync(
+        `${path.join(path.resolve(), gitName)}`,
+        `${path.join(path.resolve(), folderName)}`,
+      );
+
+      return shellJs.exit();
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
 
