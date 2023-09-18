54ce25d4ccb9d7cca44e95e38ab423d7fce4a777
oleosb <oleosbo@gmail.com>
2023-09-18 20:18:22 -0300

    Iniciando projeto 004

---
+++ 004-crawler-manipulando-arquivos-text/src/controller/crawler-palmeiras.controller.ts
@@ -0,0 +1,12 @@
+import puppeteer from 'puppeteer';
+
+export class CrawlerPalmeirasController {
+  constructor() {}
+
+  public async init() {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+
+    await page.goto('https://www.palmeiras.com.br/central-de-midia/noticias');
+  }
+}
