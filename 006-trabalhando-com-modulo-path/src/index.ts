f08bd6341e19c6e41d864f19cbcb77758a74fda2
oleosb <oleosbo@gmail.com>
2023-09-20 19:55:48 -0300

    Extensao do arquivo

--- 006-trabalhando-com-modulo-path/src/index.ts
+++ 006-trabalhando-com-modulo-path/src/index.ts
@@ -1,4 +1,3 @@
 import path from 'node:path';
 
-console.log(path.dirname(path.resolve()));
-console.log(path.basename(path.resolve('src')));
+console.log(path.extname(path.resolve('ads.controller.ts')));
