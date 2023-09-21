3d015e14d8e237e9e20a208d8c3481c3284bed6a
oleosb <oleosbo@gmail.com>
2023-09-21 19:16:56 -0300

    Criando arquvivo

--- 007-trabalhando-com-modulo-fs/src/index.ts
+++ 007-trabalhando-com-modulo-fs/src/index.ts
@@ -5,6 +5,12 @@ try {
   if (!fs.existsSync(path.resolve('tmp'))) {
     fs.mkdirSync('tmp');
   }
+
+  const caminhoArquivo = path.resolve('tmp', 'arquivo.txt');
+  if (!fs.existsSync(caminhoArquivo)) {
+    const content = 'Deu bom na criação do arquivo';
+    fs.writeFileSync(caminhoArquivo, content, 'utf-8');
+  }
 } catch (error) {
   console.log(error);
 }
