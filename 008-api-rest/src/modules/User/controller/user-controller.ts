8a8eed397196687026b2499477577ac79000ce70
oleosb <oleosbo@gmail.com>
2023-09-23 13:06:41 -0300

    Migration

--- 008-api-rest/src/modules/User/controller/user-controller.ts
+++ 008-api-rest/src/modules/User/controller/user-controller.ts
@@ -1,6 +1,20 @@
 import { Request, Response } from 'express';
+import { PrismaClient } from '@prisma/client/edge';
 
 class UserController {
+  public async create(req: Request, res: Response) {
+    const prisma = new PrismaClient();
+
+    await prisma.user.create({
+      data: {
+        email: 'oleosbo@gmail.com',
+        name: 'Leonardo Barbosa',
+      },
+    });
+
+    return res.json({ data: 'Criado com sucesso!' });
+  }
+
   public read(req: Request, res: Response) {
     return res.json({ data: 'Hello World!' });
   }
--- 008-api-rest/src/modules/User/router.ts
+++ 008-api-rest/src/modules/User/router.ts
@@ -4,6 +4,7 @@ import { userController } from './controller/user-controller';
 const router = Router();
 const baseUrl = '/user';
 
+router.post(`${baseUrl}/read`, userController.create);
 router.get(`${baseUrl}/read`, userController.read);
 
 export const userRouter = router;
